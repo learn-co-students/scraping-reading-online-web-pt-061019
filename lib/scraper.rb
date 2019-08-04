6fef05f11d4279329f2485d175ba3765ed955bd4
ennalezah <hae.villareal@gmail.com>
2019-08-04 03:24:48 -0700

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,7 +1,23 @@
 require 'nokogiri'
 require 'open-uri'
 
+class Scraper 
 html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(open(html))
+
+# puts doc.css(".site-header__hero__headline").text.strip
+
+courses = doc.css(".tout__label.heading.heading--level-4")
+
+courses.each do |course|
+   puts course.text.strip
+end
+
+p doc.css(".tout__label.heading.heading--level-4")[0]
+p doc.css(".tout__label.heading.heading--level-4")[0].name
+p doc.css(".tout__label.heading.heading--level-4")[0].attributes
+
+end
 
 
 
