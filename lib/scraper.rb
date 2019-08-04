37770ab95c4dfd7159bf563842a69c252d8d9690
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-08-04 10:18:10 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,7 +1,14 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
-
-
-
+class Scraper
+  doc = Nokogiri::HTML(open("https://flatironschool.com/"))
+  
+  doc.css(".site-header__hero__headline").text.strip
+  
+  courses = doc.css(".tout__label.heading.heading--level-4")
+  
+  courses.each {|course| puts course.text.strip}
+  
+  p doc.css(".tout__label.heading.heading--level-4")[0]
+end
