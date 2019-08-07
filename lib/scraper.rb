41b32a5d1d6ea2bfa5dd0f7d1274cb9f9d990610
Chris Junker <elevatemedfit@gmail.com>
2019-08-07 13:51:35 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,4 +4,4 @@ require 'open-uri'
 html = open("https://flatironschool.com/")
 doc = Nokogiri::HTML(html)
 
-doc.css(".tout__label.heading.heading--level-4")
+courses = doc.css(".tout__label.heading.heading--level-4")
