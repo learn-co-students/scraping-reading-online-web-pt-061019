d40892ecb3e9c5f3c428ec22f8dc54a04e88d90a
Amber Taranto <ambzzzz@gmail.com>
2019-08-11 01:06:29 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,8 +2,8 @@ require 'nokogiri'
 require 'open-uri'
 
 doc = Nokogiri::HTML(open("https://flatironschool.com/"))
-doc.css(".headline-26OIBN")
+puts doc.css(".headline-26OIBN").text.strip
 
 
 # <h1 class="headline-26OIBN">Change things.</h1>
-puts doc
+# puts doc
